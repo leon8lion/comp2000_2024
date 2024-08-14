550741d603a063e98ad3f43b6a74ffb5c13a249a
leon8lion <leon.vasington@students.mq.edu.au>
2024-08-14 16:42:09 +1000

    yeah i did stuff :)

--- src/Stage.java
+++ src/Stage.java
@@ -1,23 +1,31 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class Stage {
   Grid grid;
   Actor cat;
   Actor dog;
   Actor bird;
-
+  ArrayList<Actor> actorList;
   public Stage() {
     grid = new Grid();
     cat = new Cat(grid.cellAtColRow(0, 0));
     dog = new Dog(grid.cellAtColRow(0, 15));
     bird = new Bird(grid.cellAtColRow(12, 9));
+    actorList = new ArrayList<Actor>(){
+      {
+        add(cat);
+        add(dog);
+        add(bird);
+      }
+    };
   }
 
   public void paint(Graphics g, Point mouseLoc) {
     grid.paint(g, mouseLoc);
-    cat.paint(g);
-    dog.paint(g);
-    bird.paint(g);
+    for(int i = 0; i<actorList.size();i++){
+      actorList.get(i).paint(g);
+    }
   }
 }
